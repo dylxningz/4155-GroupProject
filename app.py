bb348dcd5810acc25016284fb8cfe48b6c9a4b5c
dylxningz <dylxningz@outlook.com>
2024-09-24 11:56:43 -0400

    added login pages

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the homepage
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+# Route for login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Route for sign-up page
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
